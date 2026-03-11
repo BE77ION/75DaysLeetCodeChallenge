0b865f6a1c618f432157b25cc49b1eb574192c8c
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-11 22:24:17 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
+    int frst(vector<int>& nums, int target){
+        int left=0;
+        int right=nums.size()-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+    int lst(vector<int>& nums, int target){
+        int left=0;
+        int right=nums.size()-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]<=target){
+                ans=mid;
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        auto lb=lower_bound(nums.begin(),nums.end(),target);
-        if(lb == nums.end() || *lb != target)
-            return {-1,-1};
-        auto ub=upper_bound(nums.begin(),nums.end(),target);
-        int first=lb-nums.begin();
-        int second=ub-nums.begin()-1;
-        return {first,second};
+       int first=frst(nums,target);
+       if(first==-1||nums[first]!=target)return {-1,-1};
+       int last=lst(nums,target);
+        
+       return {first,last};
     }
 };
