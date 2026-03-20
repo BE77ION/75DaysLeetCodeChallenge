7a4df7faed4900f5f5f98391940918939f1bf703
PAWANDEEP SINGH <157210307+BE77ION@users.noreply.github.com>
2026-03-20 23:42:42 +0530

    Time: 11 ms (13.25%), Space: 124.3 MB (37.6%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,21 +10,38 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        stack<int>s;
-        ListNode*temp=head;
-        while(temp){
-            s.push(temp->val);
-            temp=temp->next;
+    ListNode* rev(ListNode*head){
+        if(head==NULL||head->next==NULL){
+            return head;
         }
-        temp=head;
-        while(temp){
-            if(temp->val!=s.top()){
+        ListNode* newhead=rev(head->next);
+        ListNode*front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newhead;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode*fast=head;
+        ListNode*slow=head;
+        if(head==NULL||head->next==NULL){
+            return head;
+        }
+        while(fast->next!=NULL&&fast->next->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        ListNode*newhead=rev(slow->next);
+        ListNode*left=head;
+        ListNode*right=newhead;
+        while(right!=NULL){
+            if(left->val!=right->val){
+                rev(newhead);
                 return false;
             }
-            temp=temp->next;
-            s.pop();
+            left=left->next;
+            right=right->next;
         }
+        rev(newhead);
         return true;
     }
 };
